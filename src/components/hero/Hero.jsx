7f95f7530069713161030f09f61065c7ed328802
SimonMunyiri <munyirisimon6@gmail.com>
2025-05-21 21:47:48 +0300

    Animations

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import car2 from "../../assets/car2.jpeg";
 import { FaArrowRight } from "react-icons/fa";
 import Navbar from "../navbar/Navbar";
-
+import { motion as Motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -16,20 +16,42 @@ const Hero = () => {
           <section className="grid grid-cols-1 md:grid-cols-2 place-items-center min-h-[800px] text-white">
             {/* Text content */}
             <div className="text-center md:text-left space-y-8">
-              <h1 className="text-5xl lg:text-7xl uppercase  font-bold leading-tight">
+              <Motion.h1
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1,
+                  duration: 1,
+                }}
+                className="text-5xl lg:text-7xl uppercase  font-bold leading-tight"
+              >
                 Honda <br />
                 <span className="uppercase text-transparent text-outline">
                   Sports EV
                 </span>
-              </h1>
-              <button className="outline-btn flex justify-center items-center gap-4 group">
+              </Motion.h1>
+              <Motion.button
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1,
+                  duration: 0.5,
+                }}
+                className="outline-btn flex justify-center items-center gap-4 group"
+              >
                 Explore
                 <FaArrowRight className="group-hover:translate-x-2 transition" />
-              </button>
+              </Motion.button>
 
               {/* Features section here */}
               <div className="bg-black/40 space-y-2 p-6 lg-max-w-[350px] !mt-[60px]">
-                <h1 className='uppercase text-xl'>Vehicle Features</h1>
+                <h1 className="uppercase text-xl">Vehicle Features</h1>
                 <p className="text-xs leading-loose">
                   Electrified vehicles offer greater environmental efficiency by
                   reducing or eliminating driving emissions, while providing
@@ -45,8 +67,15 @@ const Hero = () => {
             </div>
 
             {/* Image section */}
-            <div className="mt-10 md:mt-0">
-              <img src={car2} alt="Car Pic Hero" className="w-full max-w-md rounded h-[400px]"/>
+            <div className="relative">
+              <img
+                src={car2}
+                alt="Car Pic Hero"
+                className="w-full max-w-md rounded h-[400px]"
+              />
+
+              {/* Glowing circle */}
+              <div className="border-[20px] border-cyan-200 rounded-full h-[180px] w-[180px] absolute top-0 left-1/2 md:h-[300px] md:w-[300px] glowing-shadow"></div>
             </div>
           </section>
         </div>
